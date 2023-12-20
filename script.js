e661828572c50caa727b2733629eba2daf04cadc
Lidiya <145801817+Aaishhhaa@users.noreply.github.com>
2023-12-21 00:06:52 +0300

    changes

---
+++ script.js
@@ -0,0 +1,58 @@
+let a = 10;
+alert (a + 10);
+
+let Iphone = 2007;
+console.log (Iphone);
+
+let Creator = 'Брендан Эйх';
+console.log (Creator);
+
+let x = 10;
+let y = 2;
+alert(x + y);
+alert(x - y);
+alert(x * y);
+alert(x / y);
+
+let result = 2 ** 5;
+alert(result);
+
+a = 9;
+b = 2;
+let bum = a % b;
+alert(bum);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+const user = {
+    name: 'Aisha',
+    age: 22,
+    isAdmin: true,
+ }
+ user.cityOfResidence = 'Magas';
+
+ user.age = 23;
+
+ alert(user.age)
+
+ delete user.cityOfResidence;
+
+ let info = prompt('Какую информацию хотите узнать о пользователе?');
+ alert(user[info])
+ 
+ let Name = prompt('Как тебя зовут ?', 'Имя');
+ alert(`Привет, ${Name}!`);
+
+
+ 
+
